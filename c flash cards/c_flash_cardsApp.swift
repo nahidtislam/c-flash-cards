6c50e9d564400d61b020d35b342a4c46907f8916
Nahid <75819596+nahidtislam@users.noreply.github.com>
2023-05-20 14:24:43 +0100

    Initial Commit

---
+++ c flash cards/c_flash_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  c_flash_cardsApp.swift
+//  c flash cards
+//
+//  Created by Nahid Islam on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct c_flash_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
